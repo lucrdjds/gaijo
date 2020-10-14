56ce42756c374e2f6d3923d49b199fac168752af
Leonardo Gideão Costa Rocha <gideonveloz@gmail.com>
2020-10-14 16:12:03 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -14,7 +14,7 @@ void *consumidor_func(void *arg);
 int indice_produtor, indice_consumidor, tamanho_buffer;
 int* buffer;
 pthread_t thread1, thread2;
-sem_t sem;
+sem_t sem1, sem2;
 
 //Você deve fazer as alterações necessárias nesta função e na função
 //consumidor_func para que usem semáforos para coordenar a produção
@@ -29,12 +29,10 @@ void *produtor_func(void *arg) {
             produto = -1;          //envia produto sinlizando FIM
         else 
             produto = produzir(i); //produz um elemento normal
-        sem_wait(&sem);   
         indice_produtor = (indice_produtor + 1) % tamanho_buffer; //calcula posição próximo elemento
-        sem_post(&sem);
+        sem_wait(&sem2);
         buffer[indice_produtor] = produto; //adiciona o elemento produzido à lista
-
-        
+        sem_post(&sem1);
     }
     pthread_exit(NULL);
 }
@@ -42,9 +40,9 @@ void *produtor_func(void *arg) {
 void *consumidor_func(void *arg) {
     while (1) {
         indice_consumidor = (indice_consumidor + 1) % tamanho_buffer; //Calcula o próximo item a consumir
-        sem_wait(&sem);
+        sem_wait(&sem1); /* Semaforo 1, esta com 0 logo não tem como funcionar */
         int produto = buffer[indice_consumidor]; //obtém o item da lista
-        sem_post(&sem);
+        sem_post(&sem2);
         //Podemos receber um produto normal ou um produto especial
         // Aqui ocorre o erro de memoria
         if (produto >= 0) {
@@ -72,22 +70,21 @@ int main(int argc, char *argv[]) {
     indice_consumidor = 0;
     buffer = malloc(sizeof(int) * tamanho_buffer);
 
-    // Crie threads e o que mais for necessário para que uma produtor crie 
-    // n_itens produtos e o consumidor os consuma
-    sem_init(&sem, 0, 1);
+    /* Inicializando os semaforos */
+    sem_init(&sem1, 0, 0);
+    sem_init(&sem2, 0, 1);
 
+    /* Construção de pthreads. */
     pthread_create(&thread2, NULL, produtor_func, (void *)&n_itens);
     pthread_create(&thread1, NULL, consumidor_func, NULL);
 
     pthread_join(thread2, NULL);
     pthread_join(thread1, NULL);
-
-    sem_post(&sem);
-
-    sem_destroy(&sem);
-    //Libera memória do buffer
+    /* Destroi os semaforos. */
+    sem_destroy(&sem1);
+    sem_destroy(&sem2);
+    /* Liberando o espaço de memoria. */
     free(buffer);
-
     return 0;
 }
 
